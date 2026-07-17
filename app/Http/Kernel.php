c3f0f6974d3f0ae7325b1757b0048b28ab865e96
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-07-17 11:00:16 +0300

    feat(admin): register administrator middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -9,8 +9,6 @@ class Kernel extends HttpKernel
     /**
      * The application's global HTTP middleware stack.
      *
-     * These middleware are run during every request to your application.
-     *
      * @var array<int, class-string|string>
      */
     protected $middleware = [
@@ -49,8 +47,6 @@ class Kernel extends HttpKernel
     /**
      * The application's route middleware.
      *
-     * These middleware may be assigned to groups or used individually.
-     *
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
@@ -63,5 +59,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
     ];
 }

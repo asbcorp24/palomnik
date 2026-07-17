00e50027423309e4946bdec92848011fb58cbd47
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-07-17 19:47:36 +0300

    feat(push): add Firebase delivery configuration

--- config/palomnik.php
+++ config/palomnik.php
@@ -14,4 +14,10 @@ return [
     'privacy' => [
         'policy_version' => env('PRIVACY_POLICY_VERSION', '1.0'),
     ],
+
+    'firebase' => [
+        'enabled' => env('FIREBASE_PUSH_ENABLED', false),
+        'project_id' => env('FIREBASE_PROJECT_ID'),
+        'service_account_path' => env('FIREBASE_SERVICE_ACCOUNT_PATH'),
+    ],
 ];

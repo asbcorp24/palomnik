1c567c923d3f907f6ead2e5966828b77c518bcd9
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-07-17 20:02:01 +0300

    fix(mobile): register Firebase background handler before app start

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'src/app.dart';
+import 'src/core/push_service.dart';
 import 'src/core/session_controller.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
   final session = SessionController();
   await session.restore();
 

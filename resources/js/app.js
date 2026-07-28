478f687ea1c61ba820b62d730b8dcc1fa89ec194
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-07-28 10:24:51 +0300

    build: bundle third-party JavaScript locally

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 require('./bootstrap');
+
+const bootstrap = require('bootstrap');
+const maplibregl = require('maplibre-gl');
+const { Html5Qrcode, Html5QrcodeScanner } = require('html5-qrcode');
+const QRCode = require('qrcodejs');
+
+window.bootstrap = bootstrap;
+window.maplibregl = maplibregl.default || maplibregl;
+window.Html5Qrcode = Html5Qrcode;
+window.Html5QrcodeScanner = Html5QrcodeScanner;
+window.QRCode = QRCode.default || QRCode;

aa213ddd1be27bfe9df01c2693070537cebd997e
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-07-31 08:23:10 +0300

    feat(poi): add admin points routes

---
+++ routes/points_of_interest.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Admin\PointOfInterestController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('admin')
+    ->name('admin.')
+    ->middleware(['auth', 'verified', 'admin'])
+    ->group(function () {
+        Route::resource('points-of-interest', PointOfInterestController::class)
+            ->except('show')
+            ->parameters(['points-of-interest' => 'pointOfInterest']);
+    });

ff3b30c51d39329b78609f295f88f58b70aeaf18
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-08-01 10:33:26 +0300

    Load catalog management routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -62,6 +62,10 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/day_route.php'));
+
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/catalog_management.php'));
         });
     }
 

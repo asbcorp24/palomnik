d186400c05ac8580678a31ab5d1cad0b20562b41
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-08-01 12:28:58 +0300

    Add unversioned viewport map API aliases

---
+++ routes/map_viewport_alias.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\Api\V1\MapViewportController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/map/objects', [MapViewportController::class, 'objects'])
+    ->middleware('throttle:240,1');
+
+Route::get('/map/objects/{objectId}', [MapViewportController::class, 'object'])
+    ->whereNumber('objectId')
+    ->middleware('throttle:240,1');
+
+Route::get('/map/points-of-interest', [MapViewportController::class, 'pointsOfInterest'])
+    ->middleware('throttle:240,1');
+
+Route::get('/map/points-of-interest/{pointId}', [MapViewportController::class, 'pointOfInterest'])
+    ->whereNumber('pointId')
+    ->middleware('throttle:240,1');

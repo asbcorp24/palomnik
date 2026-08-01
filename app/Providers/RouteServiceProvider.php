06a1ff0181efc3e41c6429cfa454c6a02044efa9
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-08-01 10:07:28 +0300

    Load route of the day routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -58,6 +58,10 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/information_audit.php'));
+
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/day_route.php'));
         });
     }
 

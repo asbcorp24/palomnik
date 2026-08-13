80e3199af5d337cc0aae0ff76d4f84ef5b6e3856
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-08-13 16:59:09 +0300

    Расширить список редактируемых текстов

--- app/Http/Controllers/SiteContentSettingController.php
+++ app/Http/Controllers/SiteContentSettingController.php
@@ -14,6 +14,15 @@ class SiteContentSettingController extends Controller
         foreach (array_keys(SiteContentSetting::DEFAULTS) as $key) {
             $rules[$key] = ['nullable', 'string', 'max:1500'];
         }
+        foreach ([
+            'home_eyebrow', 'home_categories_title', 'home_categories_lead',
+            'home_steps_title', 'home_steps_lead',
+            'routes_builder_title', 'routes_builder_lead',
+            'map_title', 'map_lead',
+            'footer_offline_title', 'footer_copyright_name',
+        ] as $key) {
+            $rules[$key] = ['nullable', 'string', 'max:1500'];
+        }
 
         $data = $request->validate($rules);
         SiteContentSetting::put(array_replace(SiteContentSetting::values(), $data));
